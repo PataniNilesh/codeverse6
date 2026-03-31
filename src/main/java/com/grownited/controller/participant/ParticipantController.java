95a1632729141887f4b7886a647aa550e127b46d
lahku16 <patanin899@gmail.com>
2026-03-31 17:23:51 +0530

    ParticipantProfile.jsp-created

--- src/main/java/com/grownited/controller/participant/ParticipantController.java
+++ src/main/java/com/grownited/controller/participant/ParticipantController.java
@@ -8,13 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.grownited.entity.HackathonEntity;
+import com.grownited.entity.UserDetailEntity;
+import com.grownited.entity.UserEntity;
 import com.grownited.repository.HackathonRepository;
+import com.grownited.repository.UserDetailRepository;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ParticipantController {
+
+	@Autowired
+    UserDetailRepository userDetailRepository;
 	
 	@Autowired
 	HackathonRepository hackathonRepository;
+
+    ParticipantController(UserDetailRepository userDetailRepository) {
+        this.userDetailRepository = userDetailRepository;
+    }
 	
 	@GetMapping("/participant/participant-dashboard")
 	public String participantDashboard() {
@@ -25,6 +36,24 @@ public class ParticipantController {
 	public String home(Model model) {
 		model.addAttribute("hackathon", hackathonRepository.findAll());
 		return "participant/Home";
+	}
+	
+	@GetMapping("participant/profile")
+	public String profile(String success, String error, HttpSession session, Model model) {
+		
+		UserEntity user = (UserEntity) session.getAttribute("user");
+		
+		if(user == null) {
+			return "redirect:/login";
+		}
+		
+		UserDetailEntity userDetail = userDetailRepository.findByUserId(user.getUserId()).orElse(new UserDetailEntity());
+		model.addAttribute("user", user);
+		model.addAttribute("userDetail", userDetail);
+		model.addAttribute("success", success);
+		model.addAttribute("error", error);
+		
+		return "participant/ParticipantProfile";
 	}
 	
 	@GetMapping("/participant/hackathon/{hackathonId}")
